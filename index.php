04eadf4ccc5344b60113ec6bc703e039e0793e85
NANDHINI <nandhinisns12@gmail.com>
2023-03-17 14:52:09 +0530

    updated

--- index.php
+++ index.php
@@ -11,56 +11,7 @@
   </head>
   <body>
     <?php echo $alert; ?>
-      <?php 
-        $nameErr = $emailErr = $phoneErr = "";
-        $name = $email =  $phone = $message = "";
-         
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-          if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
-          }else {
-            $name = test_input($_POST["name"]);
-              if(!preg_match("/^[a-zA-Z ]*$/", $name)) {                     //CHECK IF NAME CONTAINS ONLY LETTERS
-                $name_error = "Only Letters  and White Space Allowed";
-            }
-            }
-            
-          if (empty($_POST["email"])) {
-              $emailErr = "Email is required";
-            }else {
-              $email = test_input($_POST["email"]);
-               
-               // check if e-mail address is well-formed
-              if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format"; 
-              }
-            }
-          
-            
-          if (empty($_POST["message"])) {
-              $message = "";
-            }else {
-              $message = test_input($_POST["message"]);
-            }
-            
-          if (empty($_POST["phone"])) {
-              $phoneErr = "Phone Number is required";
-            }else {
-              $phone = test_input($_POST["phone"]);
-              if(!preg_match("/^(\d[\s-]?)?[\(\[\s-]{0-2}?\d{3}[\s-]?\d{4}$/i", $phone)) {
-                $phone_error = "Invalid Phone Number";
-              }
-         
-            }
-         }
-         
-          function test_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-          }
-      ?>
+      
     <!--contact section start-->
     <div class="contact-section">
       <div class="contact-info">
@@ -72,21 +23,23 @@
         <h2>Contact Us</h2>
         <form class="contact" action="" method="post">
           <div>
-          <input type="text" name="name" class="text-box" placeholder="Your Name" required autofocus>
-          <span class = "error"> <?php echo $nameErr;?></span>  
-        </div>
+            <input type="text" name="name" class="text-box"
+                   placeholder="Your Name" required 
+                   autofocus autocomplete>
+            </div>
           <div>
-          <input type="text" name="phone" class="text-box" placeholder="Your Phone Number" required>  
-          <span class = "error"> <?php echo $phoneErr;?></span>  
-        </div>
-          <div>
-          <input type="email" name="email" class="text-box" placeholder="Your Email" required>
-          <span class = "error"> <?php echo $emailErr;?></span>  
-        </div>
-          <div>
-          <textarea name="message" rows="5" placeholder="Your Message" required></textarea>    
+            <input type="tel" name="phone" class="text-box" 
+              placeholder="123-456-7890" 
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              autocomplete required>  
           </div>
-          <input type="submit" name="submit" class="send-btn" value="Send">
+          <div>
+            <input type="email" name="email" class="text-box" placeholder="example@gmail.com" autocomplete required>
+          </div>
+          <div>
+            <textarea name="message" rows="5" placeholder="Your Message" required></textarea>    
+          </div>
+            <input type="submit" name="submit" class="send-btn" value="Send">
         </form>
       </div>
     </div>
@@ -99,3 +52,58 @@
   </body>
 </html>
       
+
+
+
+
+
+<!-- <?php
+        /* $nameErr = $emailErr = $phoneErr = "";
+        $name = $email =  $phone = $message = ""; */
+         
+        /* if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+          }else {
+            $name = test_input($_POST["name"]);
+              if(!preg_match("/^[a-zA-Z ]*$/", $name)) {                     //CHECK IF NAME CONTAINS ONLY LETTERS
+                $name_error = "Only Letters  and White Space Allowed";
+            }
+            } */
+            
+          /* if (empty($_POST["email"])) {
+              $emailErr = "Email is required";
+            }else {
+              $email = test_input($_POST["email"]);
+               
+               // check if e-mail address is well-formed
+              if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format"; 
+              }
+            } */
+          
+            
+          /* if (empty($_POST["message"])) {
+              $message = "";
+            }else {
+              $message = test_input($_POST["message"]);
+            } */
+            
+          /* if (empty($_POST["phone"])) {
+              $phoneErr = "Phone Number is required";
+            }else {
+              $phone = test_input($_POST["phone"]);
+              if(!preg_match("/^(\d[\s-]?)?[\(\[\s-]{0-2}?\d{3}[\s-]?\d{4}$/i", $phone)) {
+                $phone_error = "Invalid Phone Number";
+              }
+         
+            }
+         } */
+         
+          /* function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+          } */
+      ?> -->
